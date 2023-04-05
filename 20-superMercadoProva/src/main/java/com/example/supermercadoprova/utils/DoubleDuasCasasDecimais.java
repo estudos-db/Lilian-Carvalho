eb0e1391ad172db78b02155c5fa50082153a4451
Lilian Carvalho <liliancarvalhotavares@gmail.com>
2023-04-05 12:26:13 -0300

    refatoração do metodo converterDoubleDoisDecimais

--- 20-superMercadoProva/src/main/java/com/example/supermercadoprova/utils/DoubleDuasCasasDecimais.java
+++ 20-superMercadoProva/src/main/java/com/example/supermercadoprova/utils/DoubleDuasCasasDecimais.java
@@ -4,12 +4,8 @@ import java.text.DecimalFormat;
 
 public abstract class DoubleDuasCasasDecimais {
 	public static double converterDoubleDoisDecimais(double precoDouble) {
-		DecimalFormat fmt = new DecimalFormat("0.00");
-		String string = fmt.format(precoDouble);
-		String[] part = string.split("[,]");
-		String string2 = part[0] + "." + part[1];
-		double preco = Double.parseDouble(string2);
-		return preco;
+		DecimalFormat df = new DecimalFormat("#0.00");
+		return Double.parseDouble(df.format(precoDouble));
 	}
 
 }
