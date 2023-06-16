b77f2d72052e043d3fb83fae7688c6aecc92109a
Lilian Carvalho <liliancarvalhotavares@gmail.com>
2023-06-16 13:37:23 -0300

    cursos design patterns

--- design-patterns/src/br/com/alura/loja/orcamento/Orcamento.java
+++ design-patterns/src/br/com/alura/loja/orcamento/Orcamento.java
@@ -1,51 +1,57 @@
 package br.com.alura.loja.orcamento;
 
 import br.com.alura.loja.orcamento.situacao.EmAnalise;
+import br.com.alura.loja.orcamento.situacao.Finalizado;
 import br.com.alura.loja.orcamento.situacao.SituacaoOrcamento;
+
 import java.math.BigDecimal;
 
 public class Orcamento {
 
-	private BigDecimal valor;
-	private int quantidadeIntes;
-	private SituacaoOrcamento situacao;
+    private BigDecimal valor;
+    private int quantidadeIntes;
+    private SituacaoOrcamento situacao;
 
-	public Orcamento(BigDecimal valor, int quantidadeIntes) {
-		this.valor = valor;
-		this.quantidadeIntes = quantidadeIntes;
-		this.situacao = new EmAnalise();
-	}
+    public Orcamento(BigDecimal valor, int quantidadeIntes) {
+        this.valor = valor;
+        this.quantidadeIntes = quantidadeIntes;
+        this.situacao = new EmAnalise();
+    }
 
-	public void aplicarDescontoExtra() {
-		BigDecimal valorDescontoExtra = this.situacao.calcularValorDescontoExtra(this);
-		this.valor = this.valor.subtract(valorDescontoExtra);
-	}
+    public void aplicarDescontoExtra() {
+        BigDecimal valorDescontoExtra = this.situacao.calcularValorDescontoExtra(this);
+        this.valor = this.valor.subtract(valorDescontoExtra);
+    }
 
-	public void aprovar() {
-		this.situacao.aprovar(this);
-	}
+    public void aprovar() {
+        this.situacao.aprovar(this);
+    }
 
-	public void reprovar() {
-		this.situacao.reprovar(this);
-	}
+    public void reprovar() {
+        this.situacao.reprovar(this);
+    }
 
-	public void finalizar() {
-		this.situacao.finalizar(this);
-	}
+    public void finalizar() {
+        this.situacao.finalizar(this);
+    }
 
-	public int getQuantidadeIntes() {
-		return quantidadeIntes;
-	}
+    public int getQuantidadeIntes() {
+        return quantidadeIntes;
+    }
 
-	public BigDecimal getValor() {
-		return valor;
-	}
+    public BigDecimal getValor() {
+        return valor;
+    }
 
-	public void setSituacao(SituacaoOrcamento situacao) {
-		this.situacao = situacao;
-	}
+    public SituacaoOrcamento getSituacao() {
+        return situacao;
+    }
 
-	public SituacaoOrcamento getSituacao() {
-		return situacao;
-	}
+    public void setSituacao(SituacaoOrcamento situacao) {
+        this.situacao = situacao;
+    }
+
+    public boolean isFinalizado() {
+        return situacao instanceof Finalizado;
+    }
 }
